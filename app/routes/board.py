3728fd039dd560ec6587db0dec1eae3a3336844b
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-05 13:43:40 +0900

    jinja2 template 설정 3 - member template

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
 
 board_router = APIRouter()
 
-@board_router.get('/list')
+@board_router.get('/list', response_class=HTMLResponse)
 def join() :
     return {'msg':'Hello, board list!'}
 
 
-@board_router.get('/write')
+@board_router.get('/write', response_class=HTMLResponse)
 def join() :
     return {'msg':'Hello, board write!'}
 
 
-@board_router.get('/view')
+@board_router.get('/view', response_class=HTMLResponse)
 def join() :
     return {'msg':'Hello, board view!'}
 
