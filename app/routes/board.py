32d6c39b16c268f2be8e99bd3ff064c5947376ba
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-07 10:12:59 +0900

    게시판 - 프론트엔드

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,19 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 board_router = APIRouter()
 
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+board_router.mount('/static', StaticFiles(directory='views/static'), name='static')
+
+
 @board_router.get('/list', response_class=HTMLResponse)
-def join() :
-    return {'msg':'Hello, board list!'}
+def list(req: Request):
+    return templates.TemplateResponse('board/list.html', {'request': req})
 
 
 @board_router.get('/write', response_class=HTMLResponse)
-def join() :
-    return {'msg':'Hello, board write!'}
+def write(req: Request):
+    return templates.TemplateResponse('board/write.html', {'request': req})
 
 
 @board_router.get('/view', response_class=HTMLResponse)
-def join() :
-    return {'msg':'Hello, board view!'}
+def view(req: Request):
+    return templates.TemplateResponse('board/view.html', {'request': req})
 
