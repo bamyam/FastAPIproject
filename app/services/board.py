e6d50c12e41eb5816c1845df03fc92c6ceddc0de
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-07 12:13:45 +0900

    게시판 리스트 - 백엔드

--- app/services/board.py
+++ app/services/board.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
 from app.dbfactory import Session
 from app.models.board import Board
@@ -19,6 +19,18 @@ class BoardService():
         # 변환된 회원정보를 member 테이블에 저장
         with Session() as sess:
             stmt = insert(Board).values(data)
+            result = sess.execute(stmt)
+            sess.commit()
+
+        return result
+
+    @staticmethod
+    def select_board():
+        with (Session() as sess):
+            stmt = select(Board.bno, Board.title, Board.userid,
+                          Board.regdate, Board.views)\
+            .order_by(Board.bno.desc())\
+            .offset(0).limit(10)
             result = sess.execute(stmt)
             sess.commit()
 
