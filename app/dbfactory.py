51906e1e366942f7623dcd398e5c09957735862c
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-02-14 13:47:05 +0900

    갤러리 2 - 백엔드

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.settings import config
-from app.models import member, board
+from app.models import member, board, gallery
 
 # sqlite 사용시 check_same_thread를 추가 - 쓰레드 사용 안 함
 engine = create_engine(config.dbconn, echo=True, connect_args={'check_same_thread':False})
@@ -12,3 +12,4 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def db_startup() :
     member.Base.metadata.create_all(engine)
     board.Base.metadata.create_all(engine)
+    gallery.Base.metadata.create_all(engine)
